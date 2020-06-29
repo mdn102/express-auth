6b91be9062187d5c8dca3748fdc723865654c096
nguyendm77 <nguyendm77@gmail.com>
2020-06-29 13:11:20 -0700

    updated code

--- index.js
+++ index.js
@@ -2,30 +2,30 @@
 require('dotenv').config();
 const Express = require('express')
 const ejsLayouts = require("express-ejs-layouts");
-
-//  require and set view engine use ejs
-// const ejsLayouts = require("express-ejs-layouts");
-//  set app to use false urlencoding
-//  set app public directory for use
-//  set app ejsLayouts for render
+// passport and custom middleware, sequelize session
+const helmet = require('helmet');
+const session = require("express-session");
+const flash = require('flash');
 
 
-
+//  app setup 
 const app = Express();
 app.use(Express.urlencoded({ extended: false}));
 app.use(Express.static(__dirname + "/public"));
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
+app.use(require('morgan')('dev'));
+app.use(helmet());
 
 
 //  ROUTES
 app.get('/', function(req,res) {
-    //  check to see if usre logged in
+    //  check to see if user logged in
     res.render('index');
 })
 
 // initialize App on Port
-app.listen(process.env.PORT || 4000, function(port) {
+app.listen(process.env.PORT || 3000, function(port) {
     console.log(`Listening to the smooth sweet sounds of port ${process.env.PORT} in the morning 🧇`);
 })
 
