e9b5d52ee5e7bb11b0d89400129414a457605ee1
nguyendm77 <nguyendm77@gmail.com>
2020-06-29 16:00:31 -0700

    alters code

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 // import middleware
-
+const flash = require('flash');
 
 //  register get route
 router.get('/register', function(req, res) {
@@ -10,7 +10,30 @@ router.get('/register', function(req, res) {
 })
 
 // regiter post route
-
+router.post('/register', function(req, res) {
+    db.user.findOrCreate({
+        where: {
+            email: req.body.email
+        }, defaults: {
+            name: req.body.name,
+            password: req.body.password
+        } 
+    }).then(function([user, created]) {
+        if (created) {
+            //  authenticate user and start authorization process
+            console.log('User created 🌟');
+            res.redirect('/');
+        } else {
+            console.log('User email already exists 🖐');
+            res.flash('error', 'Error: email already exists. Try a different other.');
+            res.redirect('auth/register');
+        }
+    }).catch(function(err) {
+        console.log(`Error found. \nMessage: ${err.message}. \nPLease review - ${err}`);
+        req.flash('error', err.message);
+        res.redirect('/auth/register');
+    })
+})
 
 //  login get route
 router.get('/login', function(req, res) {
