1d5a5244702ad086dc9497b514d5788bdf66f0a9
nguyendm77 <nguyendm77@gmail.com>
2020-06-30 21:10:10 -0700

    checking issue with serialization

--- config/ppConfig.js
+++ config/ppConfig.js
@@ -5,15 +5,23 @@ const db = require('../models');
 
 // serialize user
 passport.serializeUser(function(user, callback) {
+    //  there is serialization issue
     callback(null, user.id);
 });
-
+// passport.serializeUser(function(user, done) {
+//     done(null, user);
+//   });
+  
+  
 //  deserialized version 
 passport.deserializeUser(function(id, callback) {
     db.user.findByPk(id).then(function(user) {
         callback(null, user);
     }).catch(callback);
 }) 
+// passport.deserializeUser(function(user, done) {
+//     done(null, user);
+//   });
 
 //  config local variables/settings
 passport.use(new LocalStrategy ({
@@ -21,7 +29,7 @@ passport.use(new LocalStrategy ({
     passwordFiled: 'password'
 }, function(email, password, callback) {
     db.user.findOne({ where : { email }}).then(function(user) {
-        if (!user || !user.validPassword(password)) {
+        if (user || user.validPassword(password)) {
             callback(null, false);
         } else {
             callback(null, user);
